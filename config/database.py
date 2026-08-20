18434769c8ea168629e36f15f60d5967cafc19b3
fadhilll969 <sheyzz48@gmail.com>
2026-08-20 08:16:05 +0700

    user-manajemen-be

--- config/database.py
+++ config/database.py
@@ -9,6 +9,7 @@ db = Database()
 db.bind(
     provider="mysql",
     host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT", 3306)),
     user=os.getenv("DB_USER"),
     passwd=os.getenv("DB_PASSWORD"),
     db=os.getenv("DB_NAME")
