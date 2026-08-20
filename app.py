6f315007fea1dd22db5bc4c9fdbda21e1fd5dd30
fadhilll969 <sheyzz48@gmail.com>
2026-08-20 09:53:17 +0700

    Fix UserController query2

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 import falcon
-from waitress import serve
 
 from config.database import db
 from routes.User import add_routes
 from routes.Login import add_routes as add_login_routes
 from routes.Register import add_routes as add_register_routes
+
 
 app = falcon.App(
     middleware=[
@@ -20,8 +20,3 @@ db.generate_mapping(create_tables=True)
 add_routes(app)
 add_login_routes(app)
 add_register_routes(app)
-
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    serve(app, host="0.0.0.0", port=port)
