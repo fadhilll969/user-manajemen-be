20fa84a4e8b0babf9de595a54ad6fcb75efd670e
fadhilll969 <sheyzz48@gmail.com>
2026-08-20 08:27:59 +0700

    user-manajemen-be

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import falcon
 from waitress import serve
 
@@ -22,5 +23,5 @@ add_register_routes(app)
 
 
 if __name__ == "__main__":
-    print("Server berjalan di http://localhost:8000")
-    serve(app, host="127.0.0.1", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    serve(app, host="0.0.0.0", port=port)
